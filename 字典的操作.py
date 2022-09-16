7ad0934f7358a2f8af3ab22796d0c8f35c9264f1
Xinying Li <305823294@qq.com>
2022-09-16 11:58:50 +0800

    类型转换

--- 字典的操作.py
+++ 字典的操作.py
@@ -1,4 +1,4 @@
-dict1 = {'name':"lucy","age":18,2:"编号"}
+dict1 = {'name':"lucy","age":18,'2':"编号"}
 # 获取键的方法
 for k in dict1:
     print(k,type(k))
@@ -10,12 +10,14 @@ for v in dict1.values():
 # 对键值对进行遍历
 for k,v in dict1.items():
     print(k,v)
+list1 = []
 for i in dict1:
-    if  i == 2:
-        print('2在呢')
-        break
-    else:
-        print('2 不在')
+    list1.append(i)
+if '2' in list1:
+    print('键中含有2')
+else:
+    print('2不在键中')
+
 
 
 
