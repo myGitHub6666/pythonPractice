820778960ee19131d380a78dc2d4e9784d3f4571
Xinying Li <305823294@qq.com>
2022-10-03 23:21:57 +0800

    类型转换

---
+++ jsonload.py
@@ -0,0 +1,8 @@
+import json
+# json的读取
+with open("info1.json",encoding="utf-8") as f:
+    info = json.load(f)
+    # 得到的数据是list形式
+    print(type(info))
+    for i in info:
+        print(i.get('name'),i.get('age'),i.get("isman"),i.get("address").get("city"))
